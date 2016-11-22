59618b2eb227b89c49eabc167fae4bdd1fccfd9d
Irina Badigina <Irina Badigina>
2016-11-22 11:38:55 +0300

    v1

--- list.js
+++ list.js
@@ -1,2 +1,89 @@
+//Badigina Irina
+
 'use strict';
 
+const names = ['James', 'Oliver', 'Mike', 'Nick', 'Aaron',
+            'Artemy', 'Phillip', 'Anna', 'Carol', 'Kate'];
+const womenNames = ['Anna', 'Carol', 'Kate'];
+const jobs = ['Illustrator', 'Doctor', 'Programmer',
+            'Engineer', 'Orthodontist', 'Cartographer'];
+const maxSalary = 120;
+const minSalary = 20;
+const maxAge = 35;
+const minAge = 20;
+
+/*******************************/
+
+var people = [10];
+
+var peopleList = document.getElementsByClassName('people')[0];
+for (var i = 1; i <= 10; i++) {
+  createPerson(people);
+  appendPerson(people, i, peopleList);
+}
+
+for (var i = 0; i < peopleList.childNodes.length; i++) {
+  decoratePerson(peopleList.childNodes[i]);
+}
+
+/*******************************/
+
+function createPerson(list) {
+  list.push({
+    name: names[Math.floor(Math.random() * names.length)],
+    age: Math.floor(Math.random() * (maxAge - minAge)) + minAge,
+    job: jobs[Math.floor(Math.random() * jobs.length)],
+    salary: Math.floor(Math.random() * (maxSalary - minSalary)) + minSalary
+  });
+}
+
+function appendPerson(source, number, list) {
+
+  var newElement = document.createElement('li');
+
+  for (var key in source[number]) {
+    var elementProperty = document.createElement('span');
+    elementProperty.setAttribute('class', key);
+    elementProperty.textContent = source[number][key];
+    newElement.appendChild(elementProperty);
+  }
+
+  list.appendChild(newElement);
+}
+
+function decoratePerson(node) {
+  for (var i = 0; i < node.childNodes.length; i++) {
+    switch (node.childNodes[i].className) {
+      case 'name':
+        if (womenNames.indexOf(node.childNodes[i].textContent) >= 0)
+          node.style.fontSize = '1.5em';
+      break;
+      case 'age':
+        if (node.childNodes[i].textContent> 20 &&
+            node.childNodes[i].textContent < 27) {
+            for (var j = 0; j < node.childNodes.length; j++) {
+              if (node.childNodes[j].className == 'name')
+                  node.childNodes[j].style.fontWeight = "900";
+            }
+          }
+      break;
+      case 'job':
+        if (node.childNodes[i].textContent == 'Doctor') {
+          //not sure if border or underline was meant
+          node.style.textDecoration = 'underline';
+          node.style.borderBottom = '2px solid black';
+        }
+      break;
+      case 'salary':
+        if (node.childNodes[i].textContent < 50)
+          node.style.backgroundColor = '#EE5F5B';
+        else if (node.childNodes[i].textContent < 80)
+          node.style.backgroundColor = '#F89406';
+        else
+          node.style.backgroundColor = '#62C462';
+      break;
+      // default:
+      // break;
+    }
+  }
+}
--- time.js
+++ time.js
@@ -1,2 +1,74 @@
+//Badigina Irina
+//мне стыдно за именование, но иногда я просто не могу подобрать слов
+
 'use strict';
 
+const posProp = ['year', 'month', 'date', 'hours', 'minutes', 'seconds'];
+
+function Clock(arg1, arg2) {
+  if (arg1) {
+    if (arg1 instanceof Date) { //got date
+      this.time = arg1;
+      if (arg2) { //got prop
+        this.prop = arg2;
+      }
+      else {  //default prop
+        this.prop = ['hours', 'minutes', 'seconds'];
+      }
+    } else  {//default date, got prop
+      this.prop = arg1;
+      this.time = new Date();
+    }
+  } else { //default date, default prop
+    this.time =  new Date();
+    this.prop = ['hours', 'minutes', 'seconds'];
+  }
+
+  //separators
+  this.dm = ((this.prop.indexOf('date') >= 0 & this.prop.indexOf('month') >= 0) ?
+              '.' : ' ');
+  this.my = ((this.prop.indexOf('year') >= 0 & this.prop.indexOf('month') >= 0) ?
+              '.' : ' ');
+  this.hm = ((this.prop.indexOf('hours') >= 0 & this.prop.indexOf('minutes') >= 0) ?
+              ':' : ' ');
+  this.ms = ((this.prop.indexOf('seconds') >= 0 & this.prop.indexOf('minutes') >= 0) ?
+              ':' : ' ');
+}
+
+Clock.prototype = {
+  runClock: function() {
+    this.time.setSeconds(this.time.getSeconds() + 1);
+
+    document.querySelector('.clock').textContent = '';
+
+    if (this.prop.indexOf('date') >= 0)
+      document.querySelector('.clock').textContent += this.time.getDate();
+
+    if (this.prop.indexOf('month') >= 0)
+      document.querySelector('.clock').textContent += this.dm + (this.time.getMonth() + 1);
+
+    if (this.prop.indexOf('year') >= 0)
+      document.querySelector('.clock').textContent += this.my + (1900 + this.time.getYear());
+
+    if (this.prop.indexOf('hours') >= 0)
+      document.querySelector('.clock').textContent += ' ' + this.time.getHours();
+
+    if (this.prop.indexOf('minutes') >= 0)
+      document.querySelector('.clock').textContent += this.hm + this.time.getMinutes();
+
+    if (this.prop.indexOf('seconds') >= 0)
+      document.querySelector('.clock').textContent += this.ms + this.time.getSeconds();
+  },
+  stopClock: function() {
+    clearInterval(this.interval);
+  }
+}
+
+var clock1 = new Clock //tests
+(new Date('December 17, 1995 03:59:55'), ['year',  'hours', 'minutes']);
+// (['hours', 'seconds']);
+// (new Date(2014, 6, 14), ['year', 'month', 'seconds']);
+// ();
+
+clock1.runClock();
+setInterval(clock1.runClock.bind(clock1), 1000);
