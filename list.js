a07a092bcc49be43798e6740a83c9a680aa6c64c
Irina Badigina <Irina Badigina>
2016-12-03 15:45:55 +0300

    v2

--- list.js
+++ list.js
@@ -2,88 +2,77 @@
 
 'use strict';
 
-const names = ['James', 'Oliver', 'Mike', 'Nick', 'Aaron',
-            'Artemy', 'Phillip', 'Anna', 'Carol', 'Kate'];
-const womenNames = ['Anna', 'Carol', 'Kate'];
-const jobs = ['Illustrator', 'Doctor', 'Programmer',
-            'Engineer', 'Orthodontist', 'Cartographer'];
+const peopleNames = [
+    {   name: 'Shannon',
+        gender: 'male' },
+    {   name: 'James',
+        gender: 'male' },
+    {   name: 'Oliver',
+        gender: 'male' },
+    {   name: 'Mike',
+        gender: 'male' },
+    {   name: 'Artemy',
+        gender: 'male' },
+    {   name: 'Philip',
+        gender: 'male' },
+    {   name: 'Anna',
+        gender: 'female' },
+    {   name: 'Carol',
+        gender: 'female' },
+    {   name: 'Kate',
+        gender: 'female' },
+    {   name: 'Taylor',
+        gender: 'female' }
+];
+
+const jobs = ['illustrator', 'doctor', 'programmer',
+            'engineer', 'orthodontist', 'cartographer'];
+
 const maxSalary = 120;
 const minSalary = 20;
 const maxAge = 35;
 const minAge = 20;
 
-/*******************************/
+let people = new Array(10);
+let data = '<ol class=\"people\">';
+for (let i = 0; i < 10; i++) {
+    people[i] = createPerson();
 
-var people = [10];
+    data += '<li style=\"background-color:' + backgroundColor(people[i].salary) + ';'
+        + (people[i].gender == 'female' ? ' font-size: 1.5em;' : '')
+        + (people[i].job == 'cartographer' ? ' border-bottom: 4px solid black;' : '')
+        + '\">';
 
-var peopleList = document.getElementsByClassName('people')[0];
-for (var i = 1; i <= 10; i++) {
-  createPerson(people);
-  appendPerson(people, i, peopleList);
+    data += '<span class=\"name\"'
+            + (people[i].age > 20 && people[i].age < 27 ? ' style=\"font-weight: bold;\"' : '')
+            + '>' + people[i].name + '<\/span>';
+    data += '<span class=\"age\">' + people[i].age + '<\/span>';
+    data += '<span class=\"job\">' + people[i].job + '<\/span>';
+    data += '<span class=\"salary\">' + people[i].salary + '<\/span>';
+
+    data += '</li>';
+}
+data += '</ol>';
+
+document.querySelector('.content').innerHTML = data;
+
+/**/
+
+function backgroundColor(salary) {
+    return salary < 50 ? '#EE5F5B' : salary < 80 ? '#F89406' : '#62C462';
 }
 
-for (var i = 0; i < peopleList.childNodes.length; i++) {
-  decoratePerson(peopleList.childNodes[i]);
+function createPerson() {
+    let index = getRandom(0, peopleNames.length);
+    return  {
+        name: peopleNames[index].name,
+        gender: peopleNames[index].gender,
+        age: getRandom(minAge, maxAge),
+        job: jobs[getRandom(0, jobs.length)],
+        salary: getRandom(minSalary, maxSalary)
+    };
 }
 
-/*******************************/
-
-function createPerson(list) {
-  list.push({
-    name: names[Math.floor(Math.random() * names.length)],
-    age: Math.floor(Math.random() * (maxAge - minAge)) + minAge,
-    job: jobs[Math.floor(Math.random() * jobs.length)],
-    salary: Math.floor(Math.random() * (maxSalary - minSalary)) + minSalary
-  });
-}
-
-function appendPerson(source, number, list) {
-
-  var newElement = document.createElement('li');
-
-  for (var key in source[number]) {
-    var elementProperty = document.createElement('span');
-    elementProperty.setAttribute('class', key);
-    elementProperty.textContent = source[number][key];
-    newElement.appendChild(elementProperty);
-  }
-
-  list.appendChild(newElement);
-}
-
-function decoratePerson(node) {
-  for (var i = 0; i < node.childNodes.length; i++) {
-    switch (node.childNodes[i].className) {
-      case 'name':
-        if (womenNames.indexOf(node.childNodes[i].textContent) >= 0)
-          node.style.fontSize = '1.5em';
-      break;
-      case 'age':
-        if (node.childNodes[i].textContent> 20 &&
-            node.childNodes[i].textContent < 27) {
-            for (var j = 0; j < node.childNodes.length; j++) {
-              if (node.childNodes[j].className == 'name')
-                  node.childNodes[j].style.fontWeight = "900";
-            }
-          }
-      break;
-      case 'job':
-        if (node.childNodes[i].textContent == 'Doctor') {
-          //not sure if border or underline was meant
-          node.style.textDecoration = 'underline';
-          node.style.borderBottom = '2px solid black';
-        }
-      break;
-      case 'salary':
-        if (node.childNodes[i].textContent < 50)
-          node.style.backgroundColor = '#EE5F5B';
-        else if (node.childNodes[i].textContent < 80)
-          node.style.backgroundColor = '#F89406';
-        else
-          node.style.backgroundColor = '#62C462';
-      break;
-      // default:
-      // break;
-    }
-  }
+function getRandom(from, to) {
+    return Math.floor((Math.random() * (to - from)) + from);
 }
